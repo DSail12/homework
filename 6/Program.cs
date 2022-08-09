a97968025bf8c5e46f02d51e0212c8849a9d7afc
dsail <dsail@yandex.ru>
2022-08-09 23:49:06 +0300

    Задача 6

---
+++ 6/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 6
+// Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно без остатка).
+
+Console.WriteLine ("Пожалуйста, введите число");
+string? numberString = Console.ReadLine();
+
+
+
+var number = int.Parse(numberString!);
+
+
+
+
+if (number%2 == 0)
+{
+    Console.WriteLine("Число " + number + " - четное");
+}
+
+else
+{
+
+    Console.WriteLine("Число " + number + " - нечетное");
+}
